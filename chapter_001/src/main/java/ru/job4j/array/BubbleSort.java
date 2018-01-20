1c68365bbdb9476a1fedf24e5e478a4660c64f43
Nikoches <nikoches@gmail.com>
2018-01-20 18:00:02 +0300

    5.3. Создать программу для сортировки массива методом перестановки. [#195]

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BubbleSort {
+    public int[] sort(int[] array){
+        for (int j = 0;j<array.length;j++){
+        for(int i=1;i<array.length;i++){
+                if (array[i]<array[i-1]){
+                    int tmp = array[i];
+                    array[i]=array[i-1];
+                    array[i-1]=tmp;
+                    System.out.println(Arrays.toString(array));
+            }
+        }
+    }
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+    public class BubbleSortTest {
+        @Test
+        public void whenSortArrayWithTenElementsThenSortedArray() {
+          int[] array = {5,4,3,2,1};
+          BubbleSort b1 = new BubbleSort();
+          int [] ar = b1.sort(array);
+          int [] r1 = {1,2,3,4,5};
+            assertThat(ar, is(r1));//напишите здесь тест, проверяющий сортировку массива из 10 элементов методом пузырька, например {1, 5, 4, 2, 3, 1, 7, 8, 0, 5}.
+        }
+    }
+
