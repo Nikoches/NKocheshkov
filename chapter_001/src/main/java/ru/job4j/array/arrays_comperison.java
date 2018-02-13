935693f304ef09ca077ee4daffa0a544fcc28a06
Nikoches <nikoches@gmail.com>
2018-02-13 21:08:06 +0300

    Слияние массивов 2

--- chapter_001/src/main/java/ru/job4j/array/arrays_comperison.java
+++ chapter_001/src/main/java/ru/job4j/array/arrays_comperison.java
@@ -6,13 +6,10 @@ public class arrays_comperison {
         int i =0;   int j=0;  int index =0;
         int[] array2 = new int[a.length + b.length];
         while ((i<a.length) &&  (j<b.length)){
-            System.out.println("index="+index+" i="+i+" j="+j);
-           if ( a[i]<b[j]){
-               array2[index] = a[i]; i++; }
-               else {array2[index]=b[j];j++;}
-                    index++;}
-        while (i < a.length) {array2[index++] = a[i++];}
-        while (j < b.length) {array2[index++] = b[j++];}
+            array2[index] = a[i] < b[j]?a[i++]:b[j++];index++;
+             }
+        if (i<a.length){System.arraycopy (a,i, array2, index,a.length-i+1);}
+        else {System.arraycopy (b,j,array2,index,b.length-i+1);}
         return array2;
        }
     }
