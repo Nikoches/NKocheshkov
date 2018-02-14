017af33f675f74fa488c59d92d77df006adc5355
Nikoches <nikoches@gmail.com>
2018-02-14 19:14:54 +0300

    рефакторинг

--- chapter_001/src/main/java/ru/job4j/array/arrays_comperison.java
+++ chapter_001/src/main/java/ru/job4j/array/arrays_comperison.java
@@ -3,13 +3,12 @@ package ru.job4j.array;
 public class arrays_comperison {
 
     public int [] comp(int[] a,int[] b) {
-        int i =0;   int j=0;  int index =0;
+        int index_a =0;   int index_b=0;  int index =0;
         int[] array2 = new int[a.length + b.length];
-        while ((i<a.length) &&  (j<b.length)){
-            array2[index] = a[i] < b[j]?a[i++]:b[j++];index++;
-             }
-        if (i<a.length){System.arraycopy (a,i, array2, index,a.length-i+1);}
-        else {System.arraycopy (b,j,array2,index,b.length-i+1);}
+        while ((index_a<a.length) &&  (index_b<b.length)){
+            array2[index] = a[index_a] < b[index_b]?a[index_a++]:b[index_b++];index++;}
+        if (index_a<a.length){System.arraycopy (a,index_a, array2, index,a.length-index_a+1);}
+        else {System.arraycopy (b,index_b,array2,index,b.length-index_a+1);}
         return array2;
        }
     }
--- chapter_001/src/test/java/ru/job4j/array/arrays_comparison_test.java
+++ chapter_001/src/test/java/ru/job4j/array/arrays_comparison_test.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 public class arrays_comparison_test {
     @Test
     public void tryingto() {
-        int[] a= {1,3,5};
+        int[] a= {1,3};
         int[] b ={2,4,6};
         arrays_comperison ar = new arrays_comperison();
         int [] ar1 = ar.comp(a,b);
-        int [] r1 = {1,2,3,4,5,6};
+        int [] r1 = {1,2,3,4,6};
         assertThat(ar1, is(r1));
     }
 }
