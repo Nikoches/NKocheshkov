a64f26289c514e79c3140c73950ebd323fc7b711
Nikoches <nikoches@gmail.com>
2018-02-17 17:57:18 +0300

    Рефакторинг3

--- chapter_001/src/main/java/ru/job4j/array/arrays_comperison.java
+++ chapter_001/src/main/java/ru/job4j/array/arrays_comperison.java
@@ -3,12 +3,12 @@ package ru.job4j.array;
 public class arrays_comperison {
 
     public int [] comp(int[] a,int[] b) {
-        int index_a =0;   int index_b=0;  int index =0;
+        int indexFirst =0;   int indexSecond=0;  int indexThird =0;
         int[] array2 = new int[a.length + b.length];
-        while ((index_a<a.length) &&  (index_b<b.length)){
-            array2[index] = a[index_a] < b[index_b]?a[index_a++]:b[index_b++];index++;}
-        if (index_a<a.length){System.arraycopy (a,index_a, array2, index,a.length-index_a+1);}
-        else {System.arraycopy (b,index_b,array2,index,b.length-index_a+1);}
+        while ((indexFirst<a.length) &&  (indexSecond<b.length)){
+            array2[indexThird] = a[indexFirst] < b[indexSecond]?a[indexFirst++]:b[indexSecond++];indexThird++;}
+        if (indexFirst<a.length){System.arraycopy (a,indexFirst, array2, indexThird,a.length-indexFirst+1);}
+        else {System.arraycopy (b,indexSecond,array2,indexThird,b.length-indexFirst+1);}
         return array2;
        }
     }
