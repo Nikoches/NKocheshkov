a36339669d5906e8dc0b6c0c2f4b85069f59d5d3
Nikoches <nikoches@gmail.com>
2018-02-18 20:13:43 +0300

    рефакторинг 4

--- chapter_001/src/main/java/ru/job4j/array/arrays_comperison.java
+++ chapter_001/src/main/java/ru/job4j/array/arrays_comperison.java
@@ -2,14 +2,14 @@ package ru.job4j.array;
 
 public class arrays_comperison {
 
-    public int [] comp(int[] a,int[] b) {
+    public int [] comp(int[] arrayA,int[] arrayB) {
         int indexFirst =0;   int indexSecond=0;  int indexThird =0;
-        int[] array2 = new int[a.length + b.length];
-        while ((indexFirst<a.length) &&  (indexSecond<b.length)){
-            array2[indexThird] = a[indexFirst] < b[indexSecond]?a[indexFirst++]:b[indexSecond++];indexThird++;}
-        if (indexFirst<a.length){System.arraycopy (a,indexFirst, array2, indexThird,a.length-indexFirst+1);}
-        else {System.arraycopy (b,indexSecond,array2,indexThird,b.length-indexFirst+1);}
-        return array2;
+        int[] arrayFinished = new int[arrayA.length + arrayB.length];
+        while ((indexFirst<arrayA.length) &&  (indexSecond<arrayB.length)){
+            arrayFinished[indexThird] = arrayA[indexFirst] < arrayB[indexSecond]?arrayA[indexFirst++]:arrayB[indexSecond++];indexThird++;}
+        if (indexFirst<arrayA.length){System.arraycopy (arrayA,indexFirst, arrayFinished, indexThird,arrayA.length-indexFirst+1);}
+        else {System.arraycopy (arrayB,indexSecond,arrayFinished,indexThird,arrayB.length-indexFirst+1);}
+        return arrayFinished;
        }
     }
 
