834d058ea3f5940220d66adab34526afa143380b
Nikoches <nikoches@gmail.com>
2019-01-19 21:11:53 +0300

    Departmetns

--- chapter_002/src/main/java/ru/job4j/departments/Depart.java
+++ chapter_002/src/main/java/ru/job4j/departments/Depart.java
@@ -2,26 +2,25 @@ package ru.job4j.departments;
 
 
 public class Depart implements Comparable<Depart> {
-    public  Depart(String path) {
-       String names[] = path.split("\\\\");
-       this.names=names;
-       this.name = names[names.length-1];
+    public Depart(String path) {
+       String[] names = path.split("\\\\");
+       this.name = names[names.length - 1];
        this.path = path;
-       this.number = ""+(names[0].charAt(names[0].length()-1));
-       if (names.length>1) {
-       for(int i=1;i<names.length;i++){
-           this.number += names[i].charAt(names[i].length()-1);
+       this.number = "" + (names[0].charAt(names[0].length() - 1));
+       if (names.length > 1) {
+       for (int i = 1; i < names.length; i++) {
+           this.number += names[i].charAt(names[i].length() - 1);
             }
-       }else this.number = ""+names[0].charAt(names.length);
-
+       } else {
+           this.number = "" + names[0].charAt(names.length);
+       }
     }
-    public int compareTo(Depart o1){
+    public int compareTo(Depart o1) {
       // if (this.number>o1.number)
-        return 1;
+        return Integer.parseInt(this.number) - Integer.parseInt(o1.number);
     }
     protected String name;
     protected String path;
-    public String[] names;
     protected String number;
 
 }
--- chapter_002/src/main/java/ru/job4j/departments/Departments.java
+++ chapter_002/src/main/java/ru/job4j/departments/Departments.java
@@ -2,6 +2,11 @@ package ru.job4j.departments;
 
 public class Departments {
     String[] depat1 = new String[7];
+    String[] pulledDeps = pulldep();
+    String check = "";
+    int counterDepsnames = 0, lengthdepart = 0;
+    int counterDepart = 0;
+
     public String[] pulldep() {
         depat1[0] = "K1\\SK1";
         depat1[1] = "K1\\SK1\\SSK1";
@@ -11,5 +16,50 @@ public class Departments {
         depat1[5] = "K2\\SK1\\SSK1";
         depat1[6] = "K2\\SK1\\SSK2";
         return depat1;
+    }
+
+    //ВЫЧИСЛЯЕМ ДЛИННУ МАССИВА ИМЕН
+    public  Depart[] checklength() {
+        for (int i = 1; i < pulledDeps.length; i++) {
+            if (pulledDeps[i].split("\\\\").length > pulledDeps[i - 1].split("\\\\").length) {
+                check = pulledDeps[i];
+            }
+        }
+
+
+        Depart[] depsnames = new Depart[pulledDeps.length * check.split("\\\\").length];
+
+        //ЗАПОЛНЯЕМ МАССИВ НЕУНИКАЛЬНЫМИ ДЕПАРТАМЕНТАМИ
+
+        for (int i = 0; i < pulledDeps.length; i++) {
+            for (int j = 0; j < pulledDeps[i].split("\\\\").length; j++) {
+                String path = "";
+                for (int x2 = 0; x2 < pulledDeps[i].split("\\\\").length - j; x2++) {
+                    path += pulledDeps[i].split("\\\\")[x2] + "\\";
+                }
+                depsnames[counterDepsnames++] = new Depart(path);
+            }
+        }
+
+
+        //ЗАПОЛНЯЕМ УНИКАЛЬНЫМИ ДЕПАРТАМЕНТАМИ
+        for (int i = 0; i < depsnames.length; i++) {
+            for (int j = i + 1; j < depsnames.length; j++) {
+                if (depsnames[i] != null && depsnames[j] != null && depsnames[i].number.equals(depsnames[j].number)) {
+                    depsnames[j] = null;
+                    lengthdepart++;
+                }
+            }
+        }
+
+
+        //УБИРАЕМ НУЛЕВЫЕ ЭЛЕМЕНТЫ
+        Depart[] depart = new Depart[lengthdepart + 1];
+        for (Depart c : depsnames) {
+            if (c != null) {
+                depart[counterDepart++] = c;
+            }
+        }
+    return depart;
     }
 }
