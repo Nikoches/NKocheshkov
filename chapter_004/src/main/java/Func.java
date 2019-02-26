cdd05ddea2465c8e35aae1b49e2de2a2979f221e
Nikoches <nikoches@gmail.com>
2019-02-26 22:10:49 +0500

    1. Подсчет функции в диапазоне. [#24251]

---
+++ chapter_004/src/main/java/Func.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Func {
+
+
+   static List<Double> diapason(int start, int end, Function<Double, Double> func){
+        List<Double> list = new LinkedList<>();
+        for (int index = start; index != end;index++){
+            list.add(func.apply((double)index));
+        }
+        return list;
+    }
+}
---
+++ chapter_004/src/test/java/FuncTest.java
@@ -0,0 +1,27 @@
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FuncTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        List<Double> result = Func.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenSquareFunctionThenLinearResults() {
+        List<Double> result = Func.diapason(5, 8, x -> 2 * x * x + x + 3);
+        List<Double> expected = Arrays.asList(58D, 81D, 108D);
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenlogarifmicalFunctionThenLinearResults() {
+        List<Double> result = Func.diapason(5, 8, x -> Math.log(x));
+        List<Double> expected = Arrays.asList(1.6094379124341003,  1.791759469228055, 1.9459101490553132);
+        assertThat(result, is(expected));
+    }
+}
