dfafb82ece408897c9bf61ab39cb4965a65be72f
Nikoches <nikoches@gmail.com>
2019-03-18 17:57:37 +0300

    5. Преобразование матрицы чисел в список чисел. [#110228][#122615]

--- chapter_004/src/test/java/job4j/ConvertingMantrixTest.java
+++ chapter_004/src/test/java/job4j/ConvertingMantrixTest.java
@@ -2,13 +2,19 @@ package job4j;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class ConvertingMantrixTest {
 
     @Test
     public void convert(){
         ConvertingMantrix mantrix = new ConvertingMantrix();
-        mantrix.convertMatrixToList();
         Integer [][] array = {{1,2}, {3,4}};
         System.out.println(mantrix.converting(array));
+        List<Integer> matrix = List.of(1,2,3,4);
+        assertThat(mantrix.converting(array), is (matrix));
     }
 }
