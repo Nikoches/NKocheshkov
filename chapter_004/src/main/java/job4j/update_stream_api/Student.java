6a69204b798f1e1dab67d18aa06b87f308c761d8
Nikoches <nikoches@gmail.com>
2019-03-25 22:22:28 +0300

    0. Stream API улучшения [#58167]

---
+++ chapter_004/src/main/java/job4j/update_stream_api/Student.java
@@ -0,0 +1,42 @@
+package job4j.update_stream_api;
+
+import java.util.Comparator;
+public class Student implements Comparator<Student> {
+
+    private String name;
+    private int scope;
+    public Student(String name, int scope){
+        this.name = name;
+        this.scope = scope;
+    }
+        @Override
+        public int compare (Student a, Student b){
+
+            return b.getScope() - a.getScope();
+        }
+        @Override
+        public boolean equals(final Object obj) {
+            if (obj == null) {
+                return false;
+         }
+            final Student std = (Student) obj;
+            if (this == std) {
+             return true;
+            } else {
+                return (this.name.equals(std.name) && (this.scope == std.scope));
+            }
+    }
+    @Override
+    public int hashCode() {
+        int hashno = 7;
+        hashno = 13 * hashno + (name == null ? 0 : name.hashCode());
+        return hashno;
+    }
+
+    public int getScope(){
+        return this.scope;
+    }
+    public String toString(){
+        return "Name="+" " + this.name +" "+ "Scope=" + this.scope;
+    }
+}
---
+++ chapter_004/src/main/java/job4j/update_stream_api/Students.java
@@ -0,0 +1,18 @@
+package job4j.update_stream_api;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Students {
+
+    public List<Student> levelOf(List<Student> students, int bound){
+        return students.stream()
+                .sorted()
+                .flatMap(Stream::ofNullable)
+                .takeWhile(student -> student.getScope()<= bound)
+                .collect(Collectors.toList());
+    }
+}
