0ee7ca1e950716ed123cd056a42b5dcf42d0c4b0
Nikoches <nikoches@gmail.com>
2019-04-09 18:11:35 +0300

    Stream API [#106993]

---
+++ chapter_004/src/main/java/ru/job4j/Streamex.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+import java.util.Arrays;
+
+public class Streamex {
+
+    public int getSortedAndFilter(int[] array){
+       return Arrays.stream(array)
+                .filter(x -> x%2==0)
+                .map(x -> x*x)
+                .reduce(0, (acc,x) -> acc + x );
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/StreamexTest.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+public class StreamexTest {
+    @Test
+    public void whenTesting(){
+        Streamex str = new Streamex();
+        assertThat(str.getSortedAndFilter(new int[]{0,1,2,3,4,5,6,7}), is (56));
+    }
+}
