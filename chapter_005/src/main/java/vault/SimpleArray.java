0023b17b97a635668175c254094bc7f1749d8728
Nikoches <nikoches@gmail.com>
2019-04-27 15:48:51 +0300

    clean up

--- chapter_005/src/main/java/vault/SimpleArray.java
+++ chapter_005/src/main/java/vault/SimpleArray.java
@@ -4,8 +4,9 @@ import java.util.Iterator;
 
 public class SimpleArray<T> implements Iterable<T> {
 
-private Object[] mass;
-private int pos = 0;
+    private Object[] mass;
+    private int pos = 0;
+
     public SimpleArray(int size) {
         mass = new Object[size];
     }
@@ -13,6 +14,7 @@ private int pos = 0;
     public void add(T value) {
         mass[pos++] = value;
     }
+
     public boolean set(int index, T value) {
         boolean exist = false;
         if (index <= pos) {
@@ -21,22 +23,26 @@ private int pos = 0;
         }
         return exist;
     }
+
     public T get(int index) {
         return (T) mass[index];
     }
+
     public void remove(int index) {
         if (index <= pos) {
             System.arraycopy(mass, index, mass, index + 1, mass.length - index - 1);
         }
     }
+
     public Iterator<T> iterator() {
         return new Iterator() {
             private int index = 0;
+
             @Override
-            public boolean hasNext() throws NullPointerException{
+            public boolean hasNext() throws NullPointerException {
                 boolean checker = false;
-                if (index < pos ) {
-                checker = true;
+                if (index < pos) {
+                    checker = true;
                 } else throw new NullPointerException("no,no,no");
                 return checker;
             }
@@ -44,8 +50,8 @@ private int pos = 0;
             @Override
             public T next() throws NullPointerException {
                 T res = null;
-                if(hasNext()) {
-                    res  = (T) mass[index++];
+                if (hasNext()) {
+                    res = (T) mass[index++];
                 } else throw new NullPointerException("no,no,no");
                 return res;
             }
