0f81868c0f487c8406246c882d965f716a47e318
Nikoches <nikoches@gmail.com>
2019-04-30 21:01:09 +0300

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack[#130011]

--- chapter_005/src/test/java/job4j/iterating/DynamicArray/SimpleStackTest.java
+++ chapter_005/src/test/java/job4j/iterating/DynamicArray/SimpleStackTest.java
@@ -1,5 +1,6 @@
 package job4j.iterating.DynamicArray;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.iterating.DynamicArray.SimpleStack;
 
@@ -13,8 +14,8 @@ public class SimpleStackTest {
         ss.push(12);
         ss.push(13);
         ss.push(14);
-        System.out.println(ss.poll());
-        System.out.println(ss.poll());
-        System.out.println(ss.poll());
+        assertThat(ss.poll(), is(14));
+        assertThat(ss.poll(), is(13));
+        assertThat(ss.poll(), is(12));
     }
 }
