95c9eeb4df21c786a55ece6d2a61b3b5f33ec674
Nikoches <nikoches@gmail.com>
2019-05-04 21:58:08 +0300

    1. Реализовать коллекцию Set на массиве[#130027] upd 04.05.2019

--- chapter_005/src/main/java/ru/job4j/iterating/DynamicArray/SimpleSet.java
+++ chapter_005/src/main/java/ru/job4j/iterating/DynamicArray/SimpleSet.java
@@ -3,22 +3,39 @@ package ru.job4j.iterating.DynamicArray;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
-public class SimpleSet<E>  implements Iterable<E>  {
-    public int modCount = 0;
+public class SimpleSet<E> implements Iterable<E> {
     DynArray<E> set = new DynArray<>();
-    public boolean add(E value){
-        Iterator<E> iter =  set.iterator();
-        boolean checker = true;
-        while (iter.hasNext()) {
-            if(iter.next().equals(value)) {
-                checker = false;
-                break;
+    private int mode = 0;
+
+    public boolean add(E value) {
+        boolean checker = false;
+        if (mode != 0) {
+            if (checkuniq(value)) {
+                set.add(value);
+                checker = true;
+                mode++;
             }
+        } else {
+            set.add(value);
+            mode++;
         }
         return checker;
     }
 
-
+    private boolean checkuniq(E value) {
+        Iterator<E> iter = set.iterator();
+        boolean checker = true;
+            while (iter.hasNext()) {
+                if (hashCode(iter.next()) == hashCode(value)) {
+                    checker = false;
+                    break;
+                }
+            }
+        return checker;
+    }
+    public  int hashCode(E o) {
+        return o != null ? o.hashCode() : 0;
+    }
     /**
      * Returns an iterator over elements of type {@code T}.
      *
@@ -29,7 +46,6 @@ public class SimpleSet<E>  implements Iterable<E>  {
 
 
         return new Iterator<>() {
-            private int expModCount = modCount;
             private int index = 0;
 
             @Override
@@ -45,9 +61,6 @@ public class SimpleSet<E>  implements Iterable<E>  {
             @Override
             public E next() throws NullPointerException, ConcurrentModificationException {
                 E res = null;
-                if (expModCount != modCount) {
-                    throw new ConcurrentModificationException("Modification detected");
-                }
                 if (!hasNext()) {
                     throw new NullPointerException("no,no,no");
                 }
