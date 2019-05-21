ee2479c7bf9d6701a16d1136ca54ad52508700d2
Nikoches <nikoches@gmail.com>
2019-05-21 18:01:45 +0300

    Статистику по коллекции.[#130006] upd 21.05.2019

--- chapter_005/src/main/java/ru/job4j/iterating/Analize.java
+++ chapter_005/src/main/java/ru/job4j/iterating/Analize.java
@@ -1,38 +1,40 @@
 package ru.job4j.iterating;
 
-import java.util.List;
+import java.util.*;
 
 public class Analize {
 
     public Info diff(List<User> previous, List<User> current) {
         Info info = new Info();
         int nochanges = 0;
-        for (User fir : previous) {
-            int nofinded = 0;
-            for (User sec : current) {
-                if (fir.id == sec.id) {
-                    if (!fir.name.equals(sec.name)) {
-                        info.changed++;
-                    }else nochanges++;
-                }else nofinded++;
-            }
-            info.deleted = nofinded == current.size() ? info.deleted + 1 : info.deleted;
+        Map<Integer, User> mymap = new HashMap<>();
+        for (User x : current) {
+            mymap.put(x.id, x);
         }
+        int nofinded = 0;
+        for (User previousUser : previous) {
+            if (mymap.containsKey(previousUser.id)) {
+                if (!previousUser.name.equals(mymap.get(previousUser.id).name)) {
+                    info.changed++;
+                } else nochanges++;
+            } else nofinded++;
+        }
+        info.deleted = nofinded;
         info.added = current.size() - nochanges - info.changed;
         return info;
-
     }
 
     public static class User {
         private int id;
         private String name;
+
         public User(int id, String name) {
             this.name = name;
             this.id = id;
         }
     }
 
-    public  class Info {
+    public class Info {
         int added;
         int changed;
         int deleted;
