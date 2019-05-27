6403d0f0fc9b3ed054544a4e1678edde284f91e5
Nikoches <nikoches@gmail.com>
2019-05-27 17:42:52 +0300

    3. Сканирование файловой системы.[#130048] checkstyle

--- chapter_005/src/test/java/ru/job4j/iterating/Arraycomparison.java
+++ chapter_005/src/test/java/ru/job4j/iterating/Arraycomparison.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class Arraycomparison {
     @Test
     public void masTest() {
-        ru.job4j.iterating.Arraycomparison ar = new ru.job4j.iterating.Arraycomparison();
+        Arraycomparison ar = new Arraycomparison();
         char[] ch1 = new char[]{'a', 's', 'w'};
         char[] ch2 = new char[]{'a', 's', 'w'};
         System.out.println(ar.quntity(ch1, ch2));
