98d5fe28f0a71d386d233385f73853512b9e441a
Nikoches <nikoches@gmail.com>
2019-06-09 20:05:48 +0300

    Фильтры[#129994] upd 09.06.2019

--- 11.sql
+++ 11.sql
@@ -1,16 +1,16 @@
 1.SELECT * FROM  product 
-	WHERE(SELECT id FROM type WHERE type.name = "СЫР") = product.id;
+	WHERE(SELECT id FROM type WHERE type.name = "СЫР") = id;
 2.SELECT * from product
-	WHERE(product.name LIKE 'мороженое');
-3.SELECT * FROM product
-	WHERE(MONTH(expired_date)=MONTH(CURDATE));
-4.SELECT * FROM product
-	WHERE MAX(product.price);
-5.SELECT product.quantity FROM product
-	WHERE (SELECT id FROM type WHERE type.name = "СЫР") = product.id;
-6.SELECT * FROM product
-	WHERE (SELECT id FROM type WHERE type.name = "СЫР" OR type.name = 'МОЛОКО') = product.id;
-7.SELECT type.name FROM type
-	WHERE(SELECT id FROM product WHERE product.quantity < 10) = type.id;
-8.SELECT * FROM product,type
-	WHERE product.id = type.id;
+	WHERE(product.name LIKE '%мороженое%');
+3.select * from product
+	where expiried_date between 'today' and expiried_date + interval '1 month';
+4. select * from product
+	where (select max(price) from product) = price;
+5.select quantity from product
+	where(select id from type where name='сыр')=type_id;
+6. select * from product
+	where (select id from type where name ='молоко') = type_id or (select id from type where name ='сыр') = type_id;
+7. select name from type
+	where id in (select type_id from product where quantity < 10);
+8.select *,type.name from product,type
+	where product.type_id = type.id;
