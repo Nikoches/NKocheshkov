e497be73b1fd671fbab5683bc875f1a43931649c
Nikoches <nikoches@gmail.com>
2019-06-20 12:32:36 +0300

    4.4. Liquibase. Интеграционные тесты. Proxy.[#129984] upd 20.06.2019

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerSQLTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerSQLTest.java
@@ -6,7 +6,12 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.Properties;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class TrackerSQLTest {
     public Connection init() {
         try (InputStream in = TrackerSQL.class.getClassLoader().getResourceAsStream("app.properties")) {
@@ -27,13 +32,52 @@ public class TrackerSQLTest {
     @Test
     public void createItem() throws SQLException {
         try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
-            tracker.init();
             tracker.add(new Item("name", "desc", 12));
-             //assertThat(tracker.findByName("name").size(), is(1));
+            assertThat(tracker.findByName("name").size(), is(1));
             System.out.println(tracker.findByName("name").size());
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
+    @Test
+    public void removeItem() throws SQLException {
+        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+            tracker.add(new Item("name", "desc", 12));
+            tracker.deleteItem("1");
+            assertThat(tracker.findByName("name").size(), is(0));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    public void replaceItem() throws SQLException {
+        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+            tracker.add(new Item("name", "desc", 12));
+            Item ss = new Item("aaa", "bbb", 1488);
+            ss.setId("12");
+            tracker.replace("1", ss);
+            assertThat(tracker.findById("12").getName(), is("aaa"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    public void findAll() throws SQLException {
+        try (TrackerSQL tracker = new TrackerSQL(ConnectionRollback.create(this.init()))) {
+            tracker.add(new Item("aaa", "bbb", 1222));
+            tracker.add(new Item("ccc", "ddd", 1488));
+            LinkedList<Item> list = new LinkedList<>();
+            Item ss = new Item("aaa", "bbb", 1222);
+            Item ss1 = new Item("ccc", "ddd", 1488);
+            list.add(ss);
+            list.add(ss1);
+            System.out.println(tracker.findAll().toString());
+            assertThat(tracker.findAll(), is(list));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
