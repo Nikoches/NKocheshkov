37e46fb7094f5db63a7dadf381736984e4f5a08e
Nikoches <nikoches@gmail.com>
2019-07-08 21:02:14 +0300

    1. Использую класса Calculator. Сделать класс InteractCalc.[#130067] upd 21:01

--- chapter_005/src/main/java/ru/job4j/SolidPrinc/Calculator.java
+++ chapter_005/src/main/java/ru/job4j/SolidPrinc/Calculator.java
@@ -6,50 +6,59 @@ import java.io.InputStreamReader;
 public class Calculator {
     private double prev;
 
-    private double add(double first, double second) {
-        return this.prev = first + second;
+    private void add(double first, double second) {
+         this.prev = first + second;
     }
 
-    private double subtract(double first, double second) {
-        return this.prev = first - second;
+    private void subtract(double first, double second) {
+         this.prev = first - second;
     }
 
-    private double multiple(double first, double second) {
-        return this.prev = first * second;
+    private void multiple(double first, double second) {
+         this.prev = first * second;
     }
 
-    private double div(double first, double second) {
-        return this.prev = first / second;
+    private void div(double first, double second) {
+         this.prev = first / second;
     }
 
     public void menu() {
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            do {
+           do{
                 System.out.println("Input values:");
                 String ask1 = reader.readLine();
                 String ask2 = reader.readLine();
-                double x = ask1.equals("prev")?Double.valueOf(prev):Double.valueOf(ask1);
-                double y = ask1.equals("prev")?Double.valueOf(prev):Double.valueOf(ask1);
-                    System.out.println("Act:\n1=add\n2=subsctract\n3=multiply\n4=div\n5=res");
-                    int act;
-                    act = reader.read();
+                double x = ask1.equals("prev")?prev:Double.valueOf(ask1);
+                double y = ask2.equals("prev")?prev:Double.valueOf(ask2);
+                    System.out.println("Act:\n1=add\n2=subsctract\n3=multiply\n4=div");
+                    String act;
+                    act = reader.readLine();
                     switch (act) {
-                        case 1:
+                        case "1":
                             add(x, y);
-                        case 2:
+                            break;
+                        case "2":
                             subtract(x, y);
-                        case 3:
+                            break;
+                        case "3":
                             multiple(x, y);
-                        case 4:
+                            break;
+                        case "4":
                             div(x, y);
+                            break;
                     }
-                    System.out.println("res = " + this.prev);
+                    System.out.println("res = " + this.prev+" x="+x+" y="+y);
                 System.out.println("exit?");
             } while (!reader.readLine().equals("y"));
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        Calculator ss = new Calculator();
+        ss.menu();
     }
 }
