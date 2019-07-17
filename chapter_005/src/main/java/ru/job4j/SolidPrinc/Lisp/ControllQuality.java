f9187ce896c98a7f6683e88d1356ab3cb446fd20
Nikoches <nikoches@gmail.com>
2019-07-17 20:38:02 +0300

    Поиск файлов по критерию[#130056] 17.07.2019

--- chapter_005/src/main/java/ru/job4j/SolidPrinc/Lisp/ControllQuality.java
+++ chapter_005/src/main/java/ru/job4j/SolidPrinc/Lisp/ControllQuality.java
@@ -1,24 +1,27 @@
 package ru.job4j.SolidPrinc.Lisp;
 
+import java.util.ArrayList;
+
 public class ControllQuality {
     private Storage storagtr;
     private Storage storagsh;
     private Storage storagwareh;
+    private ArrayList<Storage> storageArrayList = new ArrayList<>();
     public ControllQuality(){
-        this.storagtr = new Trash();
-        this.storagsh = new Shop();
-        this.storagwareh = new Warehouse();
+        this.storageArrayList.add(new Warehouse());
+        this.storageArrayList.add(new Shop());
+        this.storageArrayList.add(new Trash());
     }
     public void productCheck(Food prd) {
         if (prd.getQuality() < 25) {
-            storagwareh.add(prd);
+            storageArrayList.get(0).add(prd);
         } else if (25 < prd.getQuality() & prd.getQuality() < 75) {
-            storagsh.add(prd);
+            storageArrayList.get(1).add(prd);
         } else if (prd.getQuality() > 75 & prd.getQuality() < 100){
             prd.setDiscount();
-            storagsh.add(prd);
+            storageArrayList.get(1).add(prd);
         }else {
-            storagtr.add(prd);
+            storageArrayList.get(2).add(prd);
         }
     }
 }
