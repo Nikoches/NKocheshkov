b6227325d9fac147755ef8554caa6a4f56b8d99f
Nikoches <nikoches@gmail.com>
2019-07-21 20:46:12 +0300

    Поиск файлов по критерию[#130056] 21.07.2019

--- chapter_005/src/main/java/ru/job4j/javaio/ArgsFinder.java
+++ chapter_005/src/main/java/ru/job4j/javaio/ArgsFinder.java
@@ -7,80 +7,20 @@ public class ArgsFinder {
     private String key;
     private HashMap<String, String> map;
 
-    public ArgsFinder(String[] args) throws Exception {
-
-        this.args = args;
-        this.directory();
-        this.output();
-        this.typeFind();
-        this.getName();
-    }
-
-    public void directory() throws Exception {
-        String result = "";
-        for (int i = 0; i < args.length; i++) {
+    public ArgsFinder(String[] args)  {
+        for (int i =0;i<args.length;i++){
             if (args[i].equals("-d")) {
-                result = args[i + 1];
-                break;
+                map.put("-d",args[i + 1]);
+            }else if(args[i].equals("-f")) {
+                map.put("-f",args[i + 1]);
+            }else if(args[i].equals("-m")) {
+                map.put("-m",args[i + 1]);
+            }else if(args[i].equals("-o")) {
+                map.put("-o",args[i + 1]);
+            }else if(args[i].equals("-n")) {
+                map.put("-n",args[i + 1]);
             }
         }
-        if(result.equals("")) throw new Exception("null flag");
-        map.put("-d", result);
-    }
-
-    public void typeFind() throws Exception {
-        String result = "";
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-f")) {
-                result = args[i + 1];
-                map.put("-f", result);
-                break;
-            } else if (args[i].equals("-m")) {
-                result = args[i + 1];
-                map.put("-m", result);
-                break;
-            }
-        }
-        if(result.equals("")) throw new Exception("null flag");
-    }
-
-    public void output() throws Exception {
-        String result = "";
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-o")) {
-                result = args[i + 1];
-                break;
-            }
-        }
-        if(result.equals("")) throw new Exception("null flag");
-        map.put("-o", result);
-    }
-
-    public void getName() throws Exception {
-        String result = "";
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-n")) {
-                result = args[i + 1];
-                break;
-            }
-        }
-        if(result.equals("")) throw new Exception("null flag");
-        map.put("-n", result);
-
-    }
-
-    public String gettype() throws Exception {
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-f")) {
-                key = "-f";
-                break;
-            } else if (args[i].equals("-m")) {
-                key = "-m";
-                break;
-            }
-        }
-        if(key.equals(null)) throw new Exception("null flag");
-        return key;
     }
 
     public String getKey(String ax) {
