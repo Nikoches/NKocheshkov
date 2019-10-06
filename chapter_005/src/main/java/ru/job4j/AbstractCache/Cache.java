46f8731d02b1d33a536d34b916f13fc5a87fd4ce
Nikoches <nikoches@gmail.com>
2019-10-06 12:11:21 +0300

    Реализации кеша на SoftReference[#130097]

--- chapter_005/src/main/java/ru/job4j/AbstractCache/Cache.java
+++ chapter_005/src/main/java/ru/job4j/AbstractCache/Cache.java
@@ -1,51 +1,33 @@
 package ru.job4j.AbstractCache;
 
+import java.lang.ref.WeakReference;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.LinkedList;
+import java.util.HashMap;
 
 public class Cache {
-    private LinkedList<Node> listCache = new LinkedList();
     private final String path = "C:\\Projects\\chapter_005\\src\\main\\resources\\cache\\";
-    public LinkedList<Node> getListCache() {
-        return listCache;
+    private HashMap<String, WeakReference<String>> cacheMap = new HashMap<>();
+
+    public HashMap getListCache() {
+        return cacheMap;
     }
 
     public String getElement(String key) {
-            for (Node x : listCache) {
-                if (x.getKey().equals(key)) {
-                    return x.getText();
-                }
-            }
-            load(key);
+        if (cacheMap.containsKey(key)) {
+            if (cacheMap.get(key) != null) {
+                return cacheMap.get(key).get();
+            } else load(key);
+        } else load(key);
         return getElement(key);
     }
 
     private void load(String key) {
         try {
-            this.listCache.add(new Node(Files.readString(Paths.get(path + "pp.txt")), key));
+            this.cacheMap.put(key, new WeakReference<>(Files.readString(Paths.get(path + key))));
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
     }
-
-    private class Node {
-        private String text;
-        private String key;
-
-        public Node(String text, String key) {
-            this.text = text;
-            this.key = key;
-        }
-
-        public String getText() {
-            return text;
-        }
-
-        public String getKey() {
-            return key;
-        }
-    }
-
 }
