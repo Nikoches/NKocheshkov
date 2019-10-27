0fd968922de569a2e3376f1041cd9eff5dd8adc0
Nikoches <nikoches@gmail.com>
2019-10-27 12:30:27 +0300

    Spring Boot start 26.10.2019

--- spring_app/src/main/java/ru/job4j/spring_app/MainController.java
+++ spring_app/src/main/java/ru/job4j/spring_app/MainController.java
@@ -14,14 +14,6 @@ import java.util.Map;
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
-        return "greeting";
-    }
 
     @GetMapping()
     public String main(Map<String, Object> model) {
@@ -29,12 +21,25 @@ public class MainController {
         model.put("messages", messages);
         return "main";
     }
+
     @PostMapping
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String,Object> model){
-        Message message = new Message(text,tag);
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages",messages);
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String main(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
         return "main";
     }
 }
--- spring_app/src/main/java/ru/job4j/spring_app/repos/MessageRepo.java
+++ spring_app/src/main/java/ru/job4j/spring_app/repos/MessageRepo.java
@@ -3,5 +3,8 @@ package ru.job4j.spring_app.repos;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.spring_app.Domain.Message;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message,Long> {
+    List<Message> findByTag(String tag);
 }
