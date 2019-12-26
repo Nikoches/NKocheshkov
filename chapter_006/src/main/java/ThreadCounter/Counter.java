1a7acabd08c5dd0d269f7ab67a88c9455f03c77d
Nikoches <nikoches@gmail.com>
2019-12-26 18:24:10 +0300

    1. класс Count и метод int incremant()[#209384]

---
+++ chapter_006/src/main/java/ThreadCounter/Counter.java
@@ -0,0 +1,19 @@
+package ThreadCounter;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Counter {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void  increment() {
+        this.value++;
+    }
+
+    public int get() {
+        return this.value;
+    }
+
+}
