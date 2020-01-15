ef2a8250e4da8932b59c0486610df1d9c6f285d4
Nikoches <nikoches@gmail.com>
2020-01-15 10:50:12 +0300

    2. Обеспечить остановку потребителя.[#209371] 15.01.2020

---
+++ chapter_006/src/main/java/ParallelSearch/ParallelSearch.java
@@ -0,0 +1,35 @@
+package ParallelSearch;
+
+import NotifyQueue.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Thread.currentThread().interrupt();
+                }
+        );
+        consumer.start();
+        final Thread producer;
+        producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        System.out.println("from producer" + index);
+                        queue.offer(index);
+                    }
+                    while (queue.getsize() > 0) {
+                    }
+                    consumer.interrupt();
+                }
+        );
+        producer.start();
+    }
+}
